39d31821462f3f1fcf931d283f8b21cf1a9d224a
sdslys <43112117+sdslys@users.noreply.github.com>
2025-09-22 22:49:06 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
         gradlePluginPortal()
         mavenCentral()
         maven("https://api.xposed.info/")
-        maven ("https://maven.pkg.github.com/GCX-HCI/tray" )
+        maven ("https://jitpack.io" )
     }
 }
 
